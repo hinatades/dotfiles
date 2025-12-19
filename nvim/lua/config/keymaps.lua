cd65fb48b49a38d81dbc56d56bbb7c278fc61f3f
hinatades <hnttisk@gmail.com>
2025-12-20 06:21:04 +0900

    fix

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<C-j>", ":bprev<CR>", { silent = true, noremap = true, desc = "前のバッファ" })
+vim.keymap.set("n", "<C-k>", ":bnext<CR>", { silent = true, noremap = true, desc = "次のバッファ" })
